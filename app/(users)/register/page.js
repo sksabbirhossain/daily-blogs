fd70955b7a3a35a7118a03ee03aaf7d606a6795b
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-01-07 13:12:06 +0600

    created register page

--- app/(users)/register/page.js
+++ app/(users)/register/page.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
 
 export const metadata = {
-  title: "Login - Daily blogs",
+  title: "Register Accout - Daily blogs",
 };
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="flex w-full h-screen -mt-[50px] justify-center items-center">
       <div className="bg-gray-100 p-3 rounded py-10 shadow-sm shadow-[#00AAA1]/50">
         <form action="">
           <div className="space-y-3">
+            <div className="space-y-2">
+              <div>
+                <label htmlFor="" className="text-sm font-medium">
+                  Name
+                </label>
+              </div>
+              <input
+                className="border border-[#00AAA1] focus:outline-[#00AAA1] rounded px-1 py-2"
+                type="text"
+                name="name"
+                placeholder="your name here..."
+              />
+            </div>
             <div className="space-y-2">
               <div>
                 <label htmlFor="" className="text-sm font-medium">
@@ -38,20 +51,12 @@ const Login = () => {
             </div>
             <div className="pt-2">
               <button className="hover:bg-[#00AAA1] border border-[#00AAA1] rounded py-2 w-full font-medium hover:text-white bg-white duration-150 ease-in">
-                Login
-              </button>
-            </div>
-            <div className="pt-2 text-center font-bold">
-              <span> - </span>OR<span> - </span>
-            </div>
-            <div className="pt-2">
-              <button className="hover:bg-[#00AAA1] border border-[#00AAA1] rounded py-2 w-full font-medium hover:text-white bg-white duration-150 ease-in">
-                Google
+                Register
               </button>
             </div>
             <div className="pt-2">
               <p className="text-center text-xs font-medium text-[#00AAA1] capitalize">
-                <Link href="/register">Create your account</Link>
+                <Link href="/login">Login your account</Link>
               </p>
             </div>
           </div>
@@ -61,4 +66,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
