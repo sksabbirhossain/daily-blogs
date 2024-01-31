c8278be9fcbaab327107149623c1a18d114581b8
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-01-31 22:55:45 +0600

    created admin dashboard page

--- app/admin/(adminPages)/dashboard/page.js
+++ app/admin/(adminPages)/dashboard/page.js
@@ -1,18 +1,15 @@
+import TopSection from "@/components/admin/Dashboard/TopSection";
+
 export const metadata = {
   title: "Dashboard - Fastribo Blogs",
 };
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p className="pt-5">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam corporis
-        sunt voluptas possimus officia! Molestias optio impedit id mollitia
-        explicabo cupiditate quia, illum qui voluptatum quidem voluptatem,
-        accusamus natus quibusdam.
-      </p>
-    </div>
+    <section className="space-y-5">
+      {/* top section */}
+      <TopSection />
+    </section>
   );
 };
 
