58ec38156d3c11b87a9e9114ed5603af3e7cc00f
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-08-12 22:13:04 +0600

    fixed forget password redirect problem

--- app/(users)/forget-password/page.js
+++ app/(users)/forget-password/page.js
@@ -1,6 +1,7 @@
 import CheckValidEmail from "@/components/users/ForgetPassword/CheckValidEmail";
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Forget Password - fastrebo Blogs",
