503bfa01fea4c70914003b675b4e6775bf1685b2
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-09-21 23:32:35 +0600

    [feat]- created a gallery page for show random images

--- app/admin/(adminPages)/gallery/page.js
+++ app/admin/(adminPages)/gallery/page.js
@@ -1,9 +1,161 @@
-import React from 'react'
+import Image from "next/image";
 
 const page = () => {
+  const images = [
+    {
+      _id: "1",
+      url: "https://images.unsplash.com/photo-1726527612459-54b061d9c48d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "2",
+      url: "https://images.unsplash.com/photo-1726853522009-8dc4c4e306a3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "3",
+      url: "https://images.unsplash.com/photo-1726766406089-0308c800b6b2?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "4",
+      url: "https://images.unsplash.com/photo-1725714354969-15e3f95fcb72?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "5",
+      url: "https://images.unsplash.com/photo-1726533815259-8fe320ac2493?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "6",
+      url: "https://images.unsplash.com/photo-1542353436-312f0e1f67ff?q=80&w=1542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "7",
+      url: "https://images.unsplash.com/photo-1726557116827-5f2a95d57cab?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "8",
+      url: "https://images.unsplash.com/photo-1548679847-1d4ff48016c7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "9",
+      url: "https://images.unsplash.com/photo-1659363093863-2d84282cae40?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "10",
+      url: "https://images.unsplash.com/photo-1725714354969-15e3f95fcb72?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "1",
+      url: "https://images.unsplash.com/photo-1726527612459-54b061d9c48d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "2",
+      url: "https://images.unsplash.com/photo-1726853522009-8dc4c4e306a3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "3",
+      url: "https://images.unsplash.com/photo-1726766406089-0308c800b6b2?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "4",
+      url: "https://images.unsplash.com/photo-1725714354969-15e3f95fcb72?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "5",
+      url: "https://images.unsplash.com/photo-1726533815259-8fe320ac2493?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "6",
+      url: "https://images.unsplash.com/photo-1542353436-312f0e1f67ff?q=80&w=1542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "7",
+      url: "https://images.unsplash.com/photo-1726557116827-5f2a95d57cab?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "8",
+      url: "https://images.unsplash.com/photo-1548679847-1d4ff48016c7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "9",
+      url: "https://images.unsplash.com/photo-1659363093863-2d84282cae40?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "10",
+      url: "https://images.unsplash.com/photo-1725714354969-15e3f95fcb72?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "11",
+      url: "https://images.unsplash.com/photo-1726549384623-c7376c784f7a?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "12",
+      url: "https://images.unsplash.com/photo-1548679847-1d4ff48016c7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "13",
+      url: "https://images.unsplash.com/photo-1721332149371-fa99da451baa?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      _id: "14",
+      url: "https://images.unsplash.com/photo-1726855500757-658894d298eb?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
   return (
-    <div> upload all images page</div>
-  )
-}
+    <section className="mb-5 space-y-5">
+      <div className="rounded bg-gray-100 px-3 py-2 text-lg font-medium shadow">
+        Gallery
+      </div>
+      <div className="grid grid-flow-dense auto-rows-[150px] grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+        {images?.map((image, i) => (
+          <div
+            className={`group relative h-full w-full overflow-hidden rounded border border-black/20 ${i % 5 === 1 ? "sm:col-span-2 " : ""}  ${i % 5 === 0 ? "sm:row-span-2" : ""}`}
+            key={i}
+          >
+            <Image
+              src={image.url}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute top-0 flex h-full w-full cursor-pointer items-center justify-center gap-2 opacity-0 transition-all duration-700 ease-in-out group-hover:bg-black/50 group-hover:opacity-100">
+              <button className="rounded bg-[#00AAA1] px-3 py-1 font-medium capitalize text-white hover:bg-[#0bb4ac]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                  />
+                </svg>
+              </button>
+              <button className="rounded bg-red-500 px-3 py-1 font-medium capitalize text-white hover:bg-red-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
