b0167024ddc283c198f42057053accd88271a6af
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-09-28 04:01:13 +0600

    created confirm delete in blogs page

--- app/admin/(adminPages)/all-blogs/page.js
+++ app/admin/(adminPages)/all-blogs/page.js
@@ -1,10 +1,7 @@
-import {
-  deleteBlog,
-  featuredBlog,
-  getAllBlogs,
-} from "@/actions/blog/blogActions";
+import { featuredBlog, getAllBlogs } from "@/actions/blog/blogActions";
 import Image from "next/image";
 import Link from "next/link";
+import DeleteBlogButton from "./DeleteBlogButton";
 
 const AllBlogs = async () => {
   const { blogs } = await getAllBlogs();
@@ -107,23 +104,7 @@ const AllBlogs = async () => {
                         <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
                       </svg>
                     </Link>
-                    <form action={deleteBlog}>
-                      <input type="hidden" name="blogId" value={blog?._id} />
-                      <button className="font-medium text-red-700  hover:underline">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="h-6 w-6"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </button>
-                    </form>
+                    <DeleteBlogButton id={blog?._id} />
                   </td>
                 </tr>
               ))}
